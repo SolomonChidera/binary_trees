89ad9af6f238b3de6023b204f4d0ffd6daa6190b
SolomonChidera <solochidera10@gmail.com>
2024-02-02 21:47:12 +0100

    Resolved an issue

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -7,6 +7,7 @@
  * @value: the value to store in the node to be inserted
  * Return: pointer to the created node, or NULL on failure
  */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
 	if (root == NULL || !value)
